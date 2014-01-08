eda84bcf1b0a60fb8f94e0bf61c8206697eb8f40
fjsantosb <fjsantosb@gmail.com>
2014-01-09 00:05:12 +0100

    Added auto-select audio type to load

--- core/audiofile.js
+++ core/audiofile.js
@@ -5,9 +5,17 @@ var AudioFile = new function() {
 	
 	this.load = function(_audioSrc, _audioName) {
 		if(!this.getAudioDataByName(_audioName)) {
+			var _audioSrcFile;
 			var _audio = new Audio();
-			_audio.src = _audioSrc;
-			_audio.addEventListener('loadeddata', function(){Game.audioFile.counter++;});
+			for(var i = 0; i < _audioSrc.length; i++) {
+				var t = _audioSrc[i].split('.');
+				console.log(_audio.canPlayType('audio/' + (t[t.length - 1])));
+				if(_audio.canPlayType('audio/' + t[t.length - 1]) != '') {
+					_audioSrcFile = _audioSrc[i];
+				}
+			}
+			_audio.src = _audioSrcFile;
+			_audio.addEventListener('canplaythrough', function(){Game.audioFile.counter++;});
 			Game.audioFile.name.push(_audioName);
 			Game.audioFile.data.push(_audio);
 		}
