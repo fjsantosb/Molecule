f8a000dc556487309b45c8b6ed019aa7fe3a81e6
fjsantosb <fjsantosb@gmail.com>
2014-01-25 20:19:40 +0100

    Changed Message to FontText

--- core/fonttext.js
+++ core/fonttext.js
@@ -1,29 +1,25 @@
-var Message = function(_font, _game) {
+var FontText = function(_font, _game) {
 	this.game = _game;
 	this.title = null;
 	this.x = 0;
 	this.y = 0;
 	this.align = 'left';
 	this.font = _font;
-	this.fillStyle = '#FFFFFF';
+	this.color = '#FFFFFF';
 	this.baseLine = 'top';
-	this.strokeStyle = '#FFFFFF';
-	this.lineWidth = 0;
+	this.alpha = 1;
 	return this;
 };
 
-Message.prototype.draw = function() {
+FontText.prototype.draw = function() {
 	this.game.context.save();
 	if(this.font !== null) {
 		this.game.context.font = this.font;
 	}
+	this.game.context.globalAlpha = this.alpha;
 	this.game.context.textAlign = this.align;
 	this.game.context.textBaseline = this.baseLine;
-	this.game.context.fillStyle = this.fillStyle;
-	this.game.context.strokeStyle = this.strokeStyle;
-	this.game.context.lineWidth = this.lineWidth;
-	if(this.lineWidth !== 0)
-		this.game.context.strokeText(this.title, this.x, this.y);
+	this.game.context.fillStyle = this.color;
 	this.game.context.fillText(this.title, this.x, this.y);
 	this.game.context.restore();
 };
--- core/game.js
+++ core/game.js
@@ -56,7 +56,7 @@ Game.prototype.loadResources = function(_interval) {
 };
 
 Game.prototype.text = function(_font) {
-	var t = new Message(_font, this);
+	var t = new FontText(_font, this);
 	this.scene.text.push(t);
 	return t;
 };
