cec480c0df407f49821797d825bb9210e4c57d10
fjsantosb <fjsantosb@gmail.com>
2014-01-28 20:23:07 +0100

    Added Tile Object

--- core/mapfile.js
+++ core/mapfile.js
@@ -1,5 +1,6 @@
 function MapFile(_game) {
 	this.game = _game;
+	this.tile = new Tile(_game);
 };
 
 MapFile.prototype.load = function(_name) {
@@ -11,14 +12,4 @@ MapFile.prototype.load = function(_name) {
 MapFile.prototype.set = function(_map) {
 	this.game.scene.map = _map;
 	this.game.camera.set();
-};
-
-MapFile.prototype.getTile = function(_name, _x, _y) {
-	var t = this.game.scene.map.getTile(_name, _x, _y);
-	return t;
-};
-
-MapFile.prototype.clearTile = function(_name, _x, _y) {
-	var t = this.game.scene.map.clearTile(_name, _x, _y);
-	return t;
 };
---
+++ core/tile.js
@@ -0,0 +1,13 @@
+function Tile(_game) {
+	this.game = _game;
+};
+
+Tile.prototype.get = function(_name, _x, _y) {
+	var t = this.game.scene.map.getTile(_name, _x, _y);
+	return t;
+};
+
+Tile.prototype.clear = function(_name, _x, _y) {
+	var t = this.game.scene.map.clearTile(_name, _x, _y);
+	return t;
+};
