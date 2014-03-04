3a280075d363dffbb341d5bfdacc160a052fe6e9
fjsantosb <fjsantosb@gameslab.local>
2014-03-04 03:18:55 +0100

    updated

--- examples/example_02/scripts/game.js
+++ examples/example_02/scripts/game.js
@@ -1,33 +1,27 @@
-Molecule(320, 320, function (game, require) {
+Molecule({
+    width: 320,
+    height: 320
+})
+.sprite('flappy', 'assets/flappy.png', 34, 24)
+.init(function (game) {
 
-    game.assets = require('assets');
-    
-    // Create sprite
-    var sprite = game.assets.sprites.flappy;
-    
-    game.init(function () {
-    
-        // Change sprite position
-        sprite.position.x = game.width / 2;
-        sprite.position.y = game.width / 2;
-        
-        // Change sprite anchor
-        sprite.anchor.x = sprite.width / 2;
-        sprite.anchor.y = sprite.height / 2;
-        
-        // Add animation with parameters: animation name, animation frames, speed
-        sprite.animation.add('fly', [0, 1, 0, 2], 0.5);
-        
-        // Stop current animation
-        sprite.animation.stop();
-        
-        // Run animation with parameters: animation name, loop, reverse 
-        sprite.animation.run('fly', true, false);
-        
-    });
-    
-    game.update(function () {
+    var sprite = game.sprite('flappy');
 
-    });
+    // Change sprite position
+    sprite.position.x = game.width / 2;
+    sprite.position.y = game.width / 2;
+
+    // Add animation with parameters: animation name,
+    // animation frames, speed
+    sprite.animation.add('fly', [0, 1, 0, 2], 0.5);
+
+    // Stop current animation
+    sprite.animation.stop();
+
+    // Run animation with parameters: animation name,
+    // loop, reverse
+    sprite.animation.run('fly', true, false);
+
+    game.add(sprite);
 
 });
